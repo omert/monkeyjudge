a864d94723ee962d812d3e1469b3cd97f325e9df
jonah honeyman <jonah@honeyman.org>
2009-10-25 23:46:57 +0200

    starts styling

--- config/environment.rb
+++ config/environment.rb
@@ -39,3 +39,8 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+Sass::Plugin.options[:style] = :compact
+Sass::Plugin.options[:template_location] = RAILS_ROOT + "/app/stylesheets"
+Sass::Plugin.options[:css_location] = RAILS_ROOT + "/public/stylesheets/compiled"
+
